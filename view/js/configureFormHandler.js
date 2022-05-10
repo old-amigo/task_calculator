57aa2dedb7ff40fd09e8e8bb10a0f096dde47242
Куруленко Дмитрий <dmitry.gwork@gmail.com>
2022-05-10 20:05:13 +0300

    feat: на вид - работающая функциональность
    добавлено:
    заглушки с данными для селекторов
    вычисление суммы

--- view/js/configureFormHandler.js
+++ view/js/configureFormHandler.js
@@ -1,13 +1,54 @@
 Vue.createApp({
     data: () => ({
-        processors:'',
-        memory:'',
-        drives:'',
-        motherboards:'',
-        cases:'',
+        // таким образом данные задаваться не должны, это заглушка пока не работает получение с БД
+        processors: [
+            {
+                "name": "AMD Ryzen 5 5600G (6x3.9 \u0413\u0413\u0446 SMT)",
+                "cost": 2300
+            }
+        ],
+        memory: [
+            {
+                "name": "8 \u0413\u0411 DDR4 NON-ECC",
+                "cost": 240
+            }, {
+                "name": "16 \u0413\u0411 DDR4 NON-ECC",
+                "cost": 480
+            }, {
+                "name": "32 \u0413\u0411 DDR4 NON-ECC",
+                "cost": 960
+            }, {
+                "name": "64 \u0413\u0411 DDR4 NON-ECC",
+                "cost": 1920
+            }],
+        drives: [
+            {
+                "name": "240 \u0413\u0411 SSD SATA Enterprise",
+                "cost": 1200
+            }, {
+                "name": "240 \u0413\u0411 SSD SATA Enterprise",
+                "cost": 2100
+            }, {
+                "name": "800 \u0413\u0411 SSD SATA Enterprise",
+                "cost": 3200
+            }],
+        motherboards: [{"name": "AMD B550", "cost": 960}],
+        cases: [
+            {
+                "name": "2U, 1 \u0431\u043b\u043e\u043a \u043f\u0438\u0442\u0430\u043d\u0438\u044f",
+                "cost": 3900
+            }, {
+                "name": "4U, 1 \u0431\u043b\u043e\u043a \u043f\u0438\u0442\u0430\u043d\u0438\u044f",
+                "cost": 7900
+            }],
+        selectedProcessor: 2300,
+        selectedMemory: 240,
+        selectedDrive: 1200,
+        selectedMotherboard: 960,
+        selectedCase: 3900
     }),
-    methods:{
-        fetchProcessors:function (){
+    methods: {
+        fetchProcessors: function () {
             axios.get('/src/controllers/getProcessors.php').then(
                 (response) => {
                     this.processors = response.data;
@@ -15,7 +56,7 @@ Vue.createApp({
                 });
         },
 
-        fetchMemory:function (){
+        fetchMemory: function () {
             axios.get('/src/controllers/getMemory.php').then(
                 (response) => {
                     this.memory = response.data;
@@ -23,7 +64,7 @@ Vue.createApp({
                 });
         },
 
-        fetchDrives:function (){
+        fetchDrives: function () {
             axios.get('/src/controllers/getDrives.php').then(
                 (response) => {
                     this.drives = response.data;
@@ -31,7 +72,7 @@ Vue.createApp({
                 });
         },
 
-        fetchMotherboards:function (){
+        fetchMotherboards: function () {
             axios.get('/src/controllers/getMotherboards.php').then(
                 (response) => {
                     this.motherboards = response.data;
@@ -39,7 +80,7 @@ Vue.createApp({
                 });
         },
 
-        fetchCases:function (){
+        fetchCases: function () {
             axios.get('/src/controllers/getCases.php').then(
                 (response) => {
                     this.cases = response.data;
@@ -49,15 +90,16 @@ Vue.createApp({
 
 
     },
-    created:function (){
+    created: function () {
         console.log('vue is ok');
 
     },
-    mounted:function (){
-        this.fetchProcessors();
-        this.fetchMemory();
-        this.fetchDrives();
-        this.fetchMotherboards();
-        this.fetchCases();
+    mounted: function () {
+        // TODO: починить получение данных от БД
+        // this.fetchProcessors();
+        // this.fetchMemory();
+        // this.fetchDrives();
+        // this.fetchMotherboards();
+        // this.fetchCases();
     }
 }).mount(".js-form");
